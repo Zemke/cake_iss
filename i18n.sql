ca73525ea92e35f5d12e5971cbb67b92a496b8bd
Zemke <florian@zemke.io>
2022-03-02 12:28:24 +0100

    i18n.sql

---
+++ i18n.sql
@@ -0,0 +1,9 @@
+-- config/schema/i18n.sql
+
+insert into i18n (`locale`, model, `foreign_key`, `field`, content) values ('de', 'Products', 2, 'name', 'zwei');
+insert into i18n (`locale`, model, `foreign_key`, `field`, content) values ('de', 'Products', 3, 'name', 'drei');
+insert into i18n (`locale`, model, `foreign_key`, `field`, content) values ('de', 'Products', 4, 'name', 'vier');
+insert into i18n (`locale`, model, `foreign_key`, `field`, content) values ('de', 'Products', 5, 'name', 'fuenf');
+insert into i18n (`locale`, model, `foreign_key`, `field`, content) values ('de', 'Products', 6, 'name', 'sechs');
+insert into i18n (`locale`, model, `foreign_key`, `field`, content) values ('de', 'Products', 6, 'name', 'sechs');
+
